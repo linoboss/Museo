0f3cdfd88db7730d1d187b0d7938d5f924a6f44d
Lino Bossio <keko.bossio@gmail.com>
2016-03-28 21:54:25 -0430

    Solo falta agregar interactividad

---
+++ assets/datos.js
@@ -0,0 +1,80 @@
+/*Estado AYACUHO*/
+
+Ayacucho1 = {img_scr: "images/ayacucho1.png",
+titulo: "MUSEO DE LA MEMORIA  ",
+contenido: "Este museo cuenta con cuatro salas en las que se exponen fotografías, pinturas, restos de vestimentas y otros objetos relacionados a la guerra interna que vivió el Perú en la década de 1980 y cuyo máximo impacto social se produjo en Ayacucho.",
+direccion: "Urbanización Nery García Zárate, Jr. Libertad 1229",
+telefono: "(066) 317170",
+horario: "lunes a domingo de 9:00 a.m. a 1:00 p.m. y de 3:00 p.m. a 6:00 p.m."}
+          
+
+Ayacucho2 = {img_scr: "images/ayacucho2.png",
+titulo: "MUSEO DEL SITIO DE QUINUA",
+contenido: "Ubicado a 35 Km. Al noreste de Ayacucho. En la sala de exposición permanente podemos encontrar armas, uniformes, maquetas y otros objetos relacionados a la Batalla de Ayacucho. Destaca el ambiente donde se firmó la Capitulación de Ayacucho el 9 de diciembre 1824.",
+direccion: "Plaza Principal de Quinua",
+telefono: "(066) 312056 ",
+horario: "martes a domingo<br>de 9:30 a.m. a 4:30 p.m. o previa cita",
+email: "ayacucho@mcultura.gob.pe"}
+
+
+
+
+Ayacucho3 = {img_scr: "images/ayacucho3.png",
+titulo: "MUSEO DE SITIO WARI",
+contenido: "En este museo de exhibe los bienes culturales encontrados en la zona Arqueológica Monumental Wari: cerámicas, textiles, líticos entre otros e información documentada con fotografías   de la zona arqueológica.",
+direccion: "Complejo Arqueológico de Wari. Km. 23 Carretera Ayacucho - Quinua.",
+telefono: "(066) 312056",
+horario: "martes a domingo de 9.00 a.m. a 5.00 p.m.",
+email: "ayacucho@mcultura.gob.pe"}
+
+
+/*Estado CAJAMARCA*/
+
+Cajamarca1 = {img_scr: "images/cajamarca1.png",
+titulo: "MUSEO ARQUEOLÓGICO HORACIO URTEAGA.  UNIVERSIDAD NACIONAL DE CAJAMARCA",
+contenido: "Se encuentran en exhibición piezas de las diversas culturas de la región.",
+direccion: "Jr. Del Batán 289 .",
+telefono: "(076) 340440",
+horario: "lunes a viernes de 8:00 a.m. a 2:00 p.m.",
+email: "ayacucho@mcultura.gob.pe"}
+
+Cajamarca2 = {img_scr: "images/cajamarca2.png",
+titulo: "MUSEO DE ARTE RELIGIOSO DEL CONVENTO DE SAN FRANCISCO",
+contenido: "Está ubicado en el Convento de San Francisco y  reúne  una serie de pinturas de la Orden franciscana, esculturas y muebles del Período virreinal.",
+direccion: "Jr. Dos de Mayo 435 .",
+telefono: "(076) 362994",
+horario: "lunes a sábado de 9:00 a.m. a 5:00 p.m."}
+
+
+Cajamarca3 = {img_scr: "images/cajamarca3.png",
+titulo: "MUSEO KUNTUR WASI",
+contenido: "En este museo se exhiben piezas que corresponden a la Zona Arqueológica del Sitio de Kuntur Wasi, cerámica, orfebrería, líticas y óseas.  También exhibe  documentos y fotografías de los trabajos realizados por la Universidad de Tokio en el sitio arqueológico.",
+direccion: "Avenida del Museo s/n. Centro Poblado Kuntur Wasi",
+horario: "martes a domingo de 9:00 a.m. a 5:00 p.m."}
+
+/*Estado CALLAO */
+
+Callao1 = {img_scr: "images/callao1.png",
+titulo: "MUSEO DE SITIO NAVAL SUBMARINO ABTAO",
+web: "www.submarinoabtao.com",
+contenido: "Cuenta con una sala de exhibición, donde se resume la evolución histórica de los submarinos en el Perú desde inicios del siglo XX. Cuenta con una sala de proyecciones y finalmente  hay un recorrido por el interior del submarino donde se aprecia al detalle sus compartimientos y finalmente se realiza una recreación en audio de un combate.",
+direccion: "Av. Jorge Chávez 120-A.",
+telefono: "(01) 7956900",
+horario: "martes a domingo de9:30 a.m. a 4:30 p.m.",
+email: "museo.abtao@hotmail.com / reservas.abtao@hotmail.com"}
+
+Callao2 = {img_scr: "images/callao2.png",
+titulo: "MUSEO DEL EJÉRCITO FORTALEZA REAL FELIPE",
+web: "www.museodelejercito.com.pe",
+contenido: "La fortaleza del Real Felipe, edificada en el siglo XVIII, es una de las pocas obras de arquitectura militar en el país y la más grande que construyeron los españoles en América del Sur. Convertida hoy en museo, la construcción con forma pentagonal abre sus puertas y permite al visitante sumergirse en la época colonial. Ofrece un recorrido por la historia del Ejército del Perú y sus héroes. En su colección se aprecian armas de guerra, uniformes, medallas, la bandera nacional, entre otros bienes culturales.",
+direccion: "Fortaleza del Real Felipe s/n. Plaza Independencia",
+telefono: "(01) 4290532",
+horario: "lunes a domingo de 9:00 a.m. a 4:00 p.m. Incluidos feriados."}
+
+Callao3 = {img_scr: "images/callao3.png",
+titulo: "MUSEO DE LA FUERZA DE AVIACIÓN NAVAL",
+contenido: "Presenta modelos a escala de la evolución de aeronaves con las que ha contado la Fuerza Aérea del Perú.",
+direccion: "Av. Faucett s/n – Callao (antes del Aeropuerto).",
+telefono: "(01) 6137100",
+horario: "Lunes a viernes de 8:00 a.m. a 5:00 p.m. (previa cita)"}
+           
